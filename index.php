9e77b6995161565663b3b3514bb14e45c52bc3a0
Ser-Nemo <eragorn2013@yandex.ru>
2020-03-20 15:06:46 +0300

    Начало работы (хэдэр готов)

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>linto</title>
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="content header-content">
+				<div class="header-top">
+					<div class="header-top-time">
+						<span class="header-top-time-span">+7 (499) 499-19-90 / Пн - Пт 10:00 - 18:00 мск</span>
+					</div>
+					<div class="header-top-lang">
+						<a class="header-top-lang-link ru" href="#">Ru</a>
+						<a class="header-top-lang-link en active" href="#">En</a>
+					</div>
+				</div>
+				<div class="header-center">
+					<div class="header-center-left">
+						<a class="header-center-left-link" href="#">
+							<span class="header-center-left-link-span">Каталог</span>
+						</a>
+						<a class="header-center-left-link" href="#">
+							<span class="header-center-left-link-span">События</span>
+						</a>
+						<a class="header-center-left-link line" href="#">
+							<span class="header-center-left-link-span">О нас</span>
+							<i class="header-center-left-link-icon"></i>
+						</a>
+						<a class="header-center-left-link" href="#">
+							<span class="header-center-left-link-span">Быстрый заказ</span>
+						</a>
+					</div>
+					<div class="header-center-logo">
+						<a class="header-center-logo-link" href="#"><img class="header-center-logo-img" src="../img/logo/logo.png" alt="Логотип"></a>
+					</div>
+					<div class="header-center-right">
+						<a class="header-center-right-link" href="#">
+							<i class="header-center-right-link-icon geo"></i>
+							<span class="header-center-right-link-span">Москва</span>
+						</a>
+						<a class="header-center-right-link" href="#">
+							<i class="header-center-right-link-icon search"></i>
+						</a>
+						<a class="header-center-right-link" href="#">
+							<i class="header-center-right-link-icon profile"></i>
+						</a>
+						<a class="header-center-right-link" href="#">
+							<i class="header-center-right-link-icon card"></i>
+							<span class="header-center-right-link-span bold">108</span>
+						</a>
+					</div>
+				</div>
+				<nav>
+					<a class="header-nav-link active" href="#">gel polish</a>
+					<a class="header-nav-link" href="#">french collection</a>
+					<a class="header-nav-link" href="#">white collection</a>
+					<a class="header-nav-link" href="#">podo line</a>
+					<a class="header-nav-link" href="#">base</a>
+					<a class="header-nav-link" href="#">base camouflage</a>
+					<a class="header-nav-link" href="#">top</a>
+					<a class="header-nav-link" href="#">gel glitz</a>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<footer>
+		<div class="container">
+			<div class="content">
+			</div>
+		</div>
+	</footer>
+	<link rel="stylesheet" type="text/css" href="../js/scroll/jquery.jscrollpane.css">
+	<script src="../js/jquery/jquery.js"></script>
+	<script src="../js/scroll/jquery.mousewheel.js"></script>
+	<script src="../js/scroll/jquery.jscrollpane.min.js"></script>
+	<script src="../js/my.js"></script>
+</body>
+</html>
