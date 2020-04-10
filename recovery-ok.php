2e1d9f6dd4981e05a3c2787571410c84672eb131
Ser-Nemo <eragorn2013@yandex.ru>
2020-04-10 14:22:23 +0300

    Две страницы ВОССТАНОВЛЕНИЕ ПАРОЛЯ готовы с адаптацией

---
+++ recovery-ok.php
@@ -0,0 +1,24 @@
+<?php include './header.php'; ?>
+<section id="recovery-pass">
+	<div class="container">
+		<div class="content">
+			<h1 class="recovery-pass-head">Восстановление пароля</h1>
+			<p class="recovery-pass-info">Мы отправили письмо для создания нового пароля по адресу viktoria97@gmail.com </p>
+			
+			<div class="recovery-pass-buttons">
+				<a class="button-link black" href="#">
+					<span class="button-link-span">
+						<span class="button-link-span-text">Отправить код повторно</span>
+					</span>
+				</a>
+				<a class="button-link white grey-border" href="#">
+					<span class="button-link-span">
+						<span class="button-link-span-text">Войти</span>
+					</span>
+				</a>
+			</div>
+			
+		</div>
+	</div>
+</section>
+<?php include './footer.php'; ?>
---
+++ recovery-pass.php
@@ -0,0 +1,31 @@
+<?php include './header.php'; ?>
+<section id="recovery-pass">
+	<div class="container">
+		<div class="content">
+			<h1 class="recovery-pass-head">Восстановление пароля</h1>
+			<p class="recovery-pass-info">Введите в поле адрес электронной почты. Мы отправим вам сообщение по электронной почте с инструкциями по созданию нового пароля.</p>
+			<div class="recovery-pass-wrap">
+				<form class="form-field" action="#" method="post">
+					<label class="form-field-label">
+						<div class="form-field-wrap first">
+							<span class="form-field-name">E-mail</span>
+						</div>
+						<div class="form-field-wrap last">
+							<input class="form-field-input email" type="text" name="email">
+							<span class="form-field-error">Это поле является обязательным</span>
+						</div>
+					</label>					
+
+					<div class="form-field-buttons">
+						<button class="button-link black">
+							<span class="button-link-span">
+								<span class="button-link-span-text">Восстановить пароль</span>
+							</span>
+						</button>						
+					</div>					
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include './footer.php'; ?>
