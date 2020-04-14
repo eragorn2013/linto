78cdb82e7d594505e4b0527b5afc06b7ef617755
Ser-Nemo <eragorn2013@yandex.ru>
2020-04-14 11:37:36 +0300

    Страница с оформленным заказом. Без адаптации

---
+++ order-ok.php
@@ -0,0 +1,258 @@
+<?php include "./elements/header.php" ?>
+<div class="bread-crumbs">
+	<div class="container">
+		<div class="content">
+			<a class="bread-crumbs-item"  href="/">Главная</a> /
+			<span class="bread-crumbs-item active">Оформление заказа</span>
+		</div>
+	</div>	
+</div>
+<section id="order-ok">
+	<div class="container order-ok-container">
+		<div class="content">
+			<h1 class="order-ok-head">Спасибо! Ваш заказ № 45613 успешно оформлен</h1>
+			<div class="order-ok-info">
+				<p class="order-ok-info-p">Сумма Вашего заказа <span class="order-ok-info-p-bold">5920 RUB</span></p>
+				<p class="order-ok-info-p">Ваш заказ будет доставлен в пункт самовывоза компании СДЭК по адресу: <span class="order-ok-info-p-bold">г. Калуга, ул. Белкина, д. 12 ТЦ “Триумф Плаза” 2 марта 2020 года.</span></p>
+				<p class="order-ok-info-p">Скидка по купону составила <span class="order-ok-info-p-bold">186 RUB</span></p>
+				<p class="order-ok-info-p">Для подтверждения заказа <span class="order-ok-info-p-bold">наш менеджер свяжется с Вами</span> по указанному в заказе телефону или электронной почте.</p>
+			</div>
+			<div class="order-ok-button">
+				<a class="button-link black" href="#">
+					<span class="button-link-span">
+						<span class="button-link-span-text">Изменить номер телефона</span>
+					</span>
+				</a>
+			</div>
+			<div class="order-ok-list">
+				<div class="order-ok-liss-wrap">
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">Контактное лицо:</span></span>
+						<span class="order-ok-item-span">Виктория Иванова</span>
+					</div>
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">Контактный номер:</span></span>
+						<span class="order-ok-item-span">+7 (812) 336-42-42</span>
+					</div>
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">E-mail:</span></span>
+						<span class="order-ok-item-span">viktoria97@gmail.com</span>
+					</div>
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">Доставка:</span></span>
+						<span class="order-ok-item-span">СДЕК - склад</span>
+					</div>
+				</div>
+
+				<div class="order-ok-liss-wrap">
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">Адрес доставки:</span></span>
+						<span class="order-ok-item-span">г. Москва, ул. Кирова,  дом № 6, кв. 2</span>
+					</div>
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">Имя получателя:</span></span>
+						<span class="order-ok-item-span">Виктория Иванова</span>
+					</div>
+					<div class="order-ok-item">
+						<span class="order-ok-item-name"><span class="order-ok-item-name-span">Способ оплаты:</span></span>
+						<span class="order-ok-item-span">Наличными</span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="bascket" class="order-ok">
+	<div class="container bascket-container">
+		<div class="content">
+			<h2 class="bascket-head">Ваш заказ</h2>			
+			<div class="bascket-list">
+				<div class="bascket-list-head">
+					<div class="bascket-list-head-item photo"></div>
+					<div class="bascket-list-head-item name">Товар</div>
+					<div class="bascket-list-head-item price">Цена</div>
+					<div class="bascket-list-head-item count">Количество</div>
+					<div class="bascket-list-head-item sum">Сумма</div>					
+				</div>
+				<div class="bascket-list-goods">
+
+					<div class="bascket-item">
+						<div class="bascket-item-pic">							
+							<img class="bascket-item-pic-img" src="img/goods/1.png" alt="товар">
+						</div>
+						<div class="bascket-item-wrap">
+							<div class="bascket-item-name">
+								<a class="bascket-item-name-text" href="#">Gel Polish spring long name greenery</a>
+								<a class="bascket-item-name-category" href="#">Gel Polish / Arctic Winter</a>
+								<span class="bascket-item-name-amount">10 мл</span>
+							</div>
+							<div class="bascket-item-price">
+								<div class="bascket-item-price-wrap actual">
+									<span class="bascket-item-price-sum">520</span>
+									<span class="bascket-item-price-valute">rub</span>
+								</div>
+							</div>
+							<div class="bascket-item-count">
+								<div class="bascket-item-count-block">
+									<span class="bascket-item-count-wrap">
+										<span class="bascket-item-count-num">5</span>
+										<span class="bascket-item-count-span">шт</span>
+									</span>
+								</div>
+							</div>
+							<div class="bascket-item-sum">
+								<span class="bascket-item-sum-num">2600</span>
+								<span class="bascket-item-sum-valute">rub</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bascket-item">
+						<div class="bascket-item-pic">
+							<img class="bascket-item-pic-img" src="img/goods/9.png" alt="товар">
+						</div>
+						<div class="bascket-item-wrap">
+							<div class="bascket-item-name">
+								<a class="bascket-item-name-text" href="#">Antibacterial</a>
+								<a class="bascket-item-name-category" href="#">French Collection</a>
+								<span class="bascket-item-name-amount">250 мл</span>
+							</div>
+							<div class="bascket-item-price">
+								<div class="bascket-item-price-wrap actual">
+									<span class="bascket-item-price-sum">520</span>
+									<span class="bascket-item-price-valute">rub</span>
+								</div>
+							</div>
+							<div class="bascket-item-count">
+								<div class="bascket-item-count-block">
+									<span class="bascket-item-count-wrap">
+										<span class="bascket-item-count-num">1</span>
+										<span class="bascket-item-count-span">шт</span>
+									</span>
+								</div>
+							</div>
+							<div class="bascket-item-sum">
+								<span class="bascket-item-sum-num">520</span>
+								<span class="bascket-item-sum-valute">rub</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bascket-item">
+						<div class="bascket-item-pic">
+							<img class="bascket-item-pic-img" src="img/goods/1.png" alt="товар">
+						</div>
+						<div class="bascket-item-wrap">
+							<div class="bascket-item-name">
+								<a class="bascket-item-name-text" href="#">GEL POLISH 025</a>
+								<a class="bascket-item-name-category" href="#">Gel Polish / Arctic Winter</a>
+								<span class="bascket-item-name-amount">10 мл</span>
+							</div>
+							<div class="bascket-item-price">
+								<div class="bascket-item-price-wrap actual red">
+									<span class="bascket-item-price-sum">520</span>
+									<span class="bascket-item-price-valute">rub</span>
+								</div>
+								<div class="bascket-item-price-wrap old">
+									<span class="bascket-item-price-sum">720</span>
+									<span class="bascket-item-price-valute">rub</span>
+								</div>
+							</div>
+							<div class="bascket-item-count">
+								<div class="bascket-item-count-block">
+									<span class="bascket-item-count-wrap">
+										<span class="bascket-item-count-num">5</span>
+										<span class="bascket-item-count-span">шт</span>
+									</span>
+								</div>
+							</div>
+							<div class="bascket-item-sum">
+								<span class="bascket-item-sum-num">2600</span>
+								<span class="bascket-item-sum-valute">rub</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="bascket-item">
+						<div class="bascket-item-pic">
+							<img class="bascket-item-pic-img" src="img/goods/9.png" alt="товар">
+						</div>
+						<div class="bascket-item-wrap">
+							<div class="bascket-item-name">
+								<a class="bascket-item-name-text" href="#">Antibacterial</a>
+								<a class="bascket-item-name-category" href="#">French Collection</a>
+								<span class="bascket-item-name-amount">250 мл</span>
+							</div>
+							<div class="bascket-item-price">
+								<div class="bascket-item-price-wrap actual">
+									<span class="bascket-item-price-sum">520</span>
+									<span class="bascket-item-price-valute">rub</span>
+								</div>
+							</div>
+							<div class="bascket-item-count">
+								<div class="bascket-item-count-block">
+									<span class="bascket-item-count-wrap">
+										<span class="bascket-item-count-num">1</span>
+										<span class="bascket-item-count-span">шт</span>
+									</span>
+								</div>
+							</div>
+							<div class="bascket-item-sum">
+								<span class="bascket-item-sum-num">520</span>
+								<span class="bascket-item-sum-valute">rub</span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="bascket-result">				
+				<div class="bascket-result-wrap order">
+
+					<div class="bascket-result-item">
+						<span class="bascket-result-item-name">Стоимость товаров:</span>
+						<div class="bascket-result-item-wrap">
+							<span class="bascket-result-item-sum">5470</span>
+							<span class="bascket-result-item-valute">rub</span>
+						</div>
+					</div>
+
+					<div class="bascket-result-item">
+						<span class="bascket-result-item-name">Скидка:</span>
+						<div class="bascket-result-item-wrap red">
+							<span class="bascket-result-item-sum">-200</span>
+							<span class="bascket-result-item-valute">rub</span>
+						</div>
+					</div>
+
+					<div class="bascket-result-item">
+						<span class="bascket-result-item-name">Доставка:</span>
+						<div class="bascket-result-item-wrap">
+							<span class="bascket-result-item-sum">450</span>
+							<span class="bascket-result-item-valute">rub</span>
+						</div>
+					</div>
+
+					<div class="bascket-result-item">
+						<span class="bascket-result-item-name">Общий вес:</span>
+						<div class="bascket-result-item-wrap">
+							<span class="bascket-result-item-sum">0.835</span>
+							<span class="bascket-result-item-valute">кг</span>
+						</div>
+					</div>
+
+					<div class="bascket-result-item total">
+						<span class="bascket-result-item-name">итого:</span>
+						<div class="bascket-result-item-wrap">
+							<span class="bascket-result-item-sum">5920</span>
+							<span class="bascket-result-item-valute">rub</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include './elements/subscription.php'; ?>
+<?php include "./elements/footer.php" ?>
