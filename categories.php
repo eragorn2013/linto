43f5af9ce229aa439c014b1f7dd1a85dd12c26d7
Ser-Nemo <eragorn2013@yandex.ru>
2020-04-17 09:21:43 +0300

    Готова новая страница со списком категорий

---
+++ categories.php
@@ -0,0 +1,206 @@
+<?php include './elements/header.php'; ?>
+<div class="bread-crumbs">
+	<div class="container">
+		<div class="content">
+			<a class="bread-crumbs-item"  href="/">Главная</a> /
+			<span class="bread-crumbs-item active">Каталог</span>
+		</div>
+	</div>	
+</div>
+<section id="cats">
+	<div class="container">
+		<div class="content">
+			<h1 class="cats-head">КАТАЛОГ</h1>
+			<div class="cats-list">
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">GEL POLISH</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/1.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">OVER THE NUDE</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/2.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">FRENCH COLLECTION</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/3.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">WHITE COLLECTION</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/4.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">PODO LINE</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/5.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">BASE</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/6.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">BASE CAMOUFLAGE</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/7.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">TOP</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/8.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">GEL PAINT</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/9.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">GEL GLITZ</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/10.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">DEHYDRATOR</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/11.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">NONACID PRIMER</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/12.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">ANTIBACTERIAL</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/13.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">УХОД ЗА КУТИКУЛОЙ</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/14.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">УХОД ЗА РУКАМИ</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/15.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">РЕКЛАМНАЯ ПРОДУКЦИЯ</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/16.png" alt="Категория">
+					</span>
+				</a>
+
+				<a class="cats-item" href="#">
+					<span class="cats-item-name">ПОДАРОЧНЫЙ НАБОР</span>
+					<span class="cats-item-view">
+						<span class="cats-item-view-span">Посмотреть коллекцию</span>
+						<i class="cats-item-view-icon"></i>
+					</span>
+					<span class="cats-item-pic">
+						<img class="cats-item-pic-img" src="img/cats/17.png" alt="Категория">
+					</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include './elements/subscription.php'; ?>
+<?php include './elements/footer.php'; ?>
