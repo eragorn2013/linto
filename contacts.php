099fafeec63fd31174cf1d3929b9d0ec099b5f59
eragorn2013 <eragorn2013@yandex.ru>
2020-04-21 14:05:07 +0300

    Контакты. Без адаптации

---
+++ contacts.php
@@ -0,0 +1,171 @@
+<?php include './elements/header.php'; ?>
+<div class="bread-crumbs">
+	<div class="container">
+		<div class="content">
+			<a class="bread-crumbs-item"  href="/">Главная</a> /
+			<span class="bread-crumbs-item active">Контакты</span>
+		</div>
+	</div>	
+</div>
+<section id="contacts">
+	<div class="container">
+		<div class="content">
+			<h1 class="contacts-head">КОНТАКТЫ</h1>
+			<p class="contacts-info">Если у вас есть вопросы по нашей продукции или вы хотите стать нашими партнерами, вы можете обратиться по одному из предоставленных ниже контактов.</p>
+			<div class="contacts-points">
+				<div class="contacts-wrap first">
+					<div class="contacts-points-item">
+						<i class="contacts-points-item-icon email"></i>
+						<span class="contacts-points-item-head">Напишите нам:</span>
+						<span class="contacts-points-item-span">info@linto-nails.ru</span>
+					</div>
+					<div class="contacts-points-item">
+						<i class="contacts-points-item-icon phone"></i>
+						<span class="contacts-points-item-head">Позвоните нам:</span>
+						<span class="contacts-points-item-span">8 (499) 499-19-90</span>
+					</div>
+					<div class="contacts-points-item">
+						<i class="contacts-points-item-icon clock"></i>
+						<span class="contacts-points-item-head">Часы работы:</span>
+						<span class="contacts-points-item-span">Пн - Пт с 10:00 до 18:00</span>
+					</div>
+				</div>
+				<div class="contacts-wrap last">
+					<div class="contacts-points-item">
+						<i class="contacts-points-item-icon geo"></i>
+						<div class="contacts-points-item-text">
+							<span class="contacts-points-item-head">Центральный офис:</span>
+							<span class="contacts-points-item-span">г. Москва, Пресненская набережная, д. 6с2, "Москва-Сити" Башня Империя, 51 этаж, офис 5113</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="socials">
+				<span class="socials-head">Свяжитесь с нами через мессенджеры и соцсети</span>
+				<div class="socials-wrap">
+					<a class="socials-item" href="#"><i class="socials-item-icon vk"></i></a>
+					<a class="socials-item" href="#"><i class="socials-item-icon fb"></i></a>
+					<a class="socials-item" href="#"><i class="socials-item-icon tw"></i></a>
+					<a class="socials-item" href="#"><i class="socials-item-icon tg"></i></a>
+					<a class="socials-item" href="#"><i class="socials-item-icon hz"></i></a>
+				</div>				
+			</div>
+
+			<div class="contacts-list">
+				<div class="contacts-list-wrap first">
+					<h2 class="contacts-list-head">ФОРМА ОБРАТНОЙ СВЯЗИ</h2>
+					<p class="contacts-list-info">Оставьте заявку и наш менеджер свяжется с Вами в ближайшее время!</p>
+					<form class="form-field" action="#" method="post">
+						<label class="form-field-label">
+							<span class="form-field-wrap first">
+								<span class="form-field-name">Ваше имя</span>
+							</span>
+							<span class="form-field-wrap last">
+								<input class="form-field-input name" type="text" name="name">
+								<span class="form-field-error"></span>
+							</span>
+						</label>
+
+						<label class="form-field-label">
+							<span class="form-field-wrap first">
+								<span class="form-field-name">E-mail</span>
+							</span>
+							<span class="form-field-wrap last">
+								<input class="form-field-input email" type="text" name="email">
+								<span class="form-field-error"></span>
+							</span>
+						</label>					
+
+						<div class="form-field-phone">
+							<div class="form-field-select phone">
+								<span class="form-field-select-name">Телефон</span>
+								<input class="form-field-select-input form-field-phone-input-region" type="hidden" name="region" value="+7">
+								<div class="form-field-select-wrap">
+									<span class="form-field-select-span">+7</span>
+									<i class="form-field-select-arrow"></i>
+									<div class="form-field-select-modal">
+										<span class="form-field-select-modal-item" data-region="+63">+63</span>
+										<span class="form-field-select-modal-item" data-region="+105">+105</span>
+										<span class="form-field-select-modal-item" data-region="+11">+11</span>
+										<span class="form-field-select-modal-item" data-region="+1">+1</span>
+										<span class="form-field-select-modal-item" data-region="+7">+7</span>
+									</div>
+								</div>
+							</div>
+							<div class="form-field-phone-wrap last">
+								<label class="form-field-label">
+									<span class="form-field-wrap last">
+										<input class="form-field-input phone" type="text" name="phone">
+										<span class="form-field-error"></span>
+									</span>
+								</label>
+							</div>
+						</div>
+
+						<label class="form-field-label">
+							<span class="form-field-wrap first">
+								<span class="form-field-name">Аккаунты в соцсетях</span>
+							</span>
+							<span class="form-field-wrap last">
+								<input class="form-field-input accounts" type="text" name="accounts">
+								<span class="form-field-error"></span>
+							</span>
+						</label>
+						
+						<label class="form-field-label">
+							<span class="form-field-wrap first">
+								<span class="form-field-name">Ваш комментарий</span>
+							</span>
+							<span class="form-field-wrap last">
+								<textarea class="form-field-input comment" name="comment"></textarea>
+								<span class="form-field-error"></span>
+							</span>
+						</label>
+						
+						
+						<div class="form-field-buttons">
+							<button class="button-link black">
+								<span class="button-link-span">
+									<span class="button-link-span-text">ОТПРАВИТЬ</span>
+								</span>
+							</button>
+						</div>
+					</form>
+				</div>
+				<div class="contacts-list-wrap last">
+					<h2 class="contacts-list-head">РЕКВИЗИТЫ</h2>
+					<p class="contacts-list-info">ИП Севастьянов Андрей Игоревич</p>
+					<div class="contacts-list-requisites">						
+						<div class="requisites-item">
+							<span class="requisites-span bold">ОГРНИП:</span>
+							<span class="requisites-span">310503022400026</span>
+						</div>
+						<div class="requisites-item">
+							<span class="requisites-span bold">ИНН:</span>
+							<span class="requisites-span">503011300510</span>
+						</div>
+						<div class="requisites-item">
+							<span class="requisites-span bold">р/с:</span>
+							<span class="requisites-span">40802810040000043047</span>
+						</div>
+						<div class="requisites-item">
+							<span class="requisites-span bold">БАНК:</span>
+							<span class="requisites-span">ОАО “Сбербанк России” г. Москва</span>
+						</div>
+						<div class="requisites-item">
+							<span class="requisites-span bold">БИК:</span>
+							<span class="requisites-span">044525225</span>
+						</div>
+						<div class="requisites-item">
+							<span class="requisites-span bold">к/c:</span>
+							<span class="requisites-span">30101810400000000225</span>
+						</div>						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include './elements/subscription.php'; ?>
+<?php include './elements/footer.php'; ?>
