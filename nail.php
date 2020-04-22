989c9edb77f2816b47744a0e142a32727688cff1
eragorn2013 <eragorn2013@yandex.ru>
2020-04-22 12:03:10 +0300

    Страница Для nail студий готова. без адаптации

---
+++ nail.php
@@ -0,0 +1,101 @@
+<?php include './elements/header.php'; ?>
+<div class="bread-crumbs">
+	<div class="container">
+		<div class="content">
+			<a class="bread-crumbs-item" href="/">Главная</a> /
+			<span class="bread-crumbs-item active">Для Nail-студий</span>
+		</div>
+	</div>	
+</div>
+<section id="nail">
+	<div id="nail-1">
+		<div class="container">
+			<div class="content">
+				<h1 class="nail-head-h1">ПРЕДЛОЖЕНИЕ ДЛЯ NAIL-СТУДИЙ И УЧЕБНЫХ ШКОЛ</h1>
+				<p class="nail-info">Современный и высокотехнологичный бренд LiNTO™ олицетворяет качество, безопасность, инновационные технологии наряду с передовыми достижениями в области бьюти-индустрии и исключительное удобство в использовании.</p>
+			</div>
+		</div>
+	</div>
+	<div id="nail-2">
+		<div class="container">
+			<div class="content nail-2-content">
+				<h2 class="nail-head-h2">ОПТОВЫЕ СКИДКИ</h2>
+				<div class="nail-discount-list">
+					<div class="nail-discount-item">
+						<span class="nail-discount-item-percent">20%</span>
+						<span class="nail-discount-item-text">при заказе на сумму</span>
+						<span class="nail-discount-item-bold">от 20 000* RUB</span>
+						<span class="nail-discount-item-info">(на последующие заказы закрепляется базовая скидка 10%**)</span>
+					</div>
+					<div class="nail-discount-item">
+						<span class="nail-discount-item-percent">25%</span>
+						<span class="nail-discount-item-text">при заказе на сумму</span>
+						<span class="nail-discount-item-bold">от 35 000* RUB</span>
+						<span class="nail-discount-item-info">(на последующие заказы закрепляется базовая скидка 10%**)</span>
+					</div>
+				</div>
+				<p class="nail-discount-info">* - суммы указаны по рознице (до применения скидки). ** - базовая скдика предоставляется для заказов до 20 000 р., для заказов свыше предоставляется скидка по стандартным условиями.</p>
+			</div>
+		</div>
+	</div>
+	<div id="nail-3">
+		<div class="container">
+			<div class="content">
+				<h2 class="nail-head-h2">CASHBACK 3% ОТ LiNTO</h2>
+				<p class="nail-info">С каждого заказа вы получаете <span class="nail-info-bold">кэшбек-бонусы в размере 3%</span> на ваш внутренний счет. В дальнейшем <span class="nail-info-bold">вы можете потратить</span> данные бонусы <span class="nail-info-bold">на оплату до 20%</span> от стоимости будущих заказов.</p>
+			</div>
+		</div>
+	</div>
+	<div id="nail-4">
+		<div class="container">
+			<div class="content">
+				<div class="nail-points">
+					<div class="nail-points-item">
+						<i class="nail-points-item-icon gift"></i>
+						<h3 class="nail-head-h3">ПОДАРОК К ПЕРВОМУ ЗАКАЗУ</h3>
+						<p class="nail-points-item-p">премиальный блокнот с наклейками на типсы (при наличии гель-лаков в заказе)</p>
+					</div>
+					<div class="nail-points-item">
+						<i class="nail-points-item-icon car"></i>
+						<h3 class="nail-head-h3">3 РАБОЧИХ ДНЯ</h3>
+						<p class="nail-points-item-p">максимальный срок отправки товара после оплаты</p>
+					</div>
+					<div class="nail-points-item">
+						<i class="nail-points-item-icon sert"></i>
+						<h3 class="nail-head-h3">СЕРТИФИКАТЫ КАЧЕСТВА</h3>
+						<p class="nail-points-item-p">и заключение о гипоаллергенности гель-лаков на все товары</p>
+					</div>
+					<div class="nail-points-item">
+						<i class="nail-points-item-icon test"></i>
+						<h3 class="nail-head-h3">ФОРМУЛА 11-FREE</h3>
+						<p class="nail-points-item-p">не содержит раздражающие и аллергенные хим. вещества</p>
+					</div>
+				</div>
+				<div class="nail-items">
+					<div class="nail-items-wrap first">
+						<h4 class="nail-head-h4">ВАС ЗАИНТЕРЕСУЕТ ПАРТНЕРСТВО С  LINTO, ЕСЛИ ВЫ:</h4>
+						<ul class="nail-ul">
+							<li class="nail-li">Вы специализируетесь на высококачественной маникюрной продукции и уходовых средств для рук и ногтей.</li>
+							<li class="nail-li">Вы работаете с широким ассортиментом бьюти-продукции.</li>
+							<li class="nail-li">Вы ищите материалы premium класса для маникюра и ухода за руками и ногтями, обеспечивающие любые потребности клиента.</li>
+							<li class="nail-li">Вы стремитесь к большему и никогда не останавливайтесь на достигнутом.</li>
+						</ul>
+					</div>
+					<div class="nail-items-wrap last">
+						<h4 class="nail-head-h4">ПОЗВОЛЬТЕ СВОЕМУ БИЗНЕСУ ВЫЙТИ НА НОВЫЙ УРОВЕНЬ!</h4>
+						<ul class="nail-ul">
+							<li class="nail-li">В основе продукции LiNTO™ лежат инновационные разработки европейских инженеров-технологов, трудившихся несколько лет над созданием запатентованной формулы нашей продукции.</li>
+							<li class="nail-li">Наша продукция PREMIUM класса соответствует самым высоким стандартам качества, что подтверждается необходимыми декларациями соответствия и протоколами испытаний.</li>
+							<li class="nail-li">Линейка продуктов включат в себя более 100 наименований.</li>
+							<li class="nail-li">Мгновенная адаптация к продукции для мастеров, комфорт и великолепный результат для клиентов.</li>
+							<li class="nail-li">Оптовый партнёр может продавать только уходовые средства бренда LiNTO™</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include './elements/subscription.php'; ?>
+<?php include './elements/footer.php'; ?>
