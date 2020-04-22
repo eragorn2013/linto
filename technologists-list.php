6dad18984bbf90fbd107a95f43596fcc16f4f881
eragorn2013 <eragorn2013@yandex.ru>
2020-04-22 17:01:41 +0300

    Страница со списком технологов готова. Без адаптации

---
+++ technologists-list.php
@@ -0,0 +1,82 @@
+<?php include "./elements/header.php" ?>
+<div class="bread-crumbs">
+	<div class="container">
+		<div class="content">
+			<a class="bread-crumbs-item"  href="/">Главная</a> /
+			<span class="bread-crumbs-item active">Технологи</span>
+		</div>
+	</div>	
+</div>
+<section id="tech-list">
+	<div class="container">
+		<div class="content tech-content">
+			<h1 class="tech-list-head">ИНСТРУКТОРА - ТЕХНОЛОГИ LINTO</h1>
+			<div class="tech-list-wrap">
+				<div class="tech-item">
+					<div class="tech-avatar">
+						<img class="tech-avatar-img" src="img/technologists/1.png" alt="">
+					</div>
+					<div class="tech-info">
+						<div class="tech-info-head">
+							<h2 class="tech-info-head-h">ЗАВОРОТЫНСКАЯ ОЛЬГА</h2>
+							<span class="tech-info-head-span">Инструктор-технолог Linto, г. Москва</span>
+						</div>
+						<div class="tech-info-socials">
+							<a target="_blank" class="tech-info-socials-link vk" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link fb" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link inst" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link wp" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link skype" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link tg" href="#"></a>
+						</div>
+						<p class="tech-info-preview">
+							12 лет в nail индустрии. Более 9 лет в салонном бизнесе. Прошла путь от мастера «домушника» до владельца сети салонов полного цикла.
+						</p>
+						<div class="quote">
+							<i class="quote-icon"></i>
+							<p class="quote-p">12 лет в nail индустрии. Более 9 лет в салонном бизнесе. Прошла путь от мастера «домушника» до владельца сети салонов полного цикла.</p>
+						</div>
+						<div class="tech-list-more">
+							<a class="tech-list-link" href="#">
+								<span class="tech-list-more-span">Подробнее</span>
+								<i class="tech-list-more-icon"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="tech-item">
+					<div class="tech-avatar">
+						<img class="tech-avatar-img" src="img/technologists/2.png" alt="">
+					</div>
+					<div class="tech-info">
+						<div class="tech-info-head">
+							<h2 class="tech-info-head-h">СТАНОВАЯ СВЕТЛАНА</h2>
+							<span class="tech-info-head-span">Инструктор-технолог Linto, г. Минск</span>
+						</div>
+						<div class="tech-info-socials">
+							<a target="_blank" class="tech-info-socials-link vk" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link fb" href="#"></a>
+							<a target="_blank" class="tech-info-socials-link inst" href="#"></a>
+						</div>
+						<p class="tech-info-preview">
+							В профессии с 2015 года. Практикующий мастер и инструктор международного уровня. Перфекционист, вдохновитель и мотиватор, переворачивающий сознание и меняющий отношение к профессии в целом. Обучила искусству маникюра более 1000 мастеров.
+						</p>
+						<div class="quote">
+							<i class="quote-icon"></i>
+							<p class="quote-p">Финальный штрих при создании образа, такой же незаменимый для совершенного стиля аксессуар...</p>
+						</div>
+						<div class="tech-list-more">
+							<a class="tech-list-link" href="#">
+								<span class="tech-list-more-span">Подробнее</span>
+								<i class="tech-list-more-icon"></i>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php include './elements/subscription.php'; ?>
+<?php include './elements/footer.php'; ?>
