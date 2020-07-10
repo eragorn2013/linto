e6b5324e2879882052c4394a698f22422a3f2f17
eragorn2013 <eragorn2013@yandex.ru>
2020-07-10 10:06:19 +0300

    Сделал 404 страницу

---
+++ 404.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>404 Страницы не существует</title>
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:500" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Titillium+Web:700,900" rel="stylesheet">
+	<link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<div id="notfound">
+		<div class="notfound">
+			<div class="notfound-logo"><img src="img/logo/logo.png" alt="Логотип"></div>
+			<div class="notfound-404">				
+				<h1 class="notfound-404-head">404</h1>
+			</div>
+			<h2 class="notfound-404-text">К сожалению этой страницы не существует!</h2>			
+			<a class="notfound-404-button" href="#">Вернуться на сайт</a>
+			<div class="notfound-info">
+				<div class="notfound-info-wrap">
+					<i class="notfound-info-icon phone"></i>
+					<span class="notfound-info-text">+7 (499) 499-19-90</span>
+				</div>
+				<div class="notfound-info-wrap">
+					<i class="notfound-info-icon address"></i>
+					<span class="notfound-info-text">г. Москва, Пресненская набережная, д. 6с2, "Москва-Сити" Башня Империя, 51 этаж, офис 5113</span>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
