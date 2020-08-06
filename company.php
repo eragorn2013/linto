2bf7d14ce911904626a870b8f83efbc36a6d2844
eragorn2013 <eragorn2013@yandex.ru>
2020-08-06 12:06:27 +0300

    Сделана страница КОМПАНИЯ

---
+++ company.php
@@ -0,0 +1,45 @@
+<?php include './elements/header.php'; ?>
+<div class="bread-crumbs">
+	<div class="container">
+		<div class="content">
+			<a class="bread-crumbs-item"  href="/">Главная</a> /
+			<span class="bread-crumbs-item active">Компания</span>
+		</div>
+	</div>	
+</div>
+<section id="company">
+	<div class="container company-container">
+		<div class="content company-content">
+			<div class="company-block">
+				<h2 class="company-left">
+					<span class="company-left-span top">LiNTO™</span>
+					<span class="company-left-span bottom">БЫЛА СОЗДАНА</span>
+				</h2>
+				<p class="company-right">в 2016 году командой из профессиональных технологов и бьюти-специалистов, решивших объеденить под общим брендом самые современные разработки в области маникюра и индустрии красоты.</p>
+			</div>
+			<div class="company-block">
+				<h2 class="company-left">
+					<span class="company-left-span top">ОСНОВНАЯ</span>
+					<span class="company-left-span bottom">СПЕЦИАЛИЗАЦИЯ</span>
+				</h2>
+				<p class="company-right">производство гель-лаков для ногтей и сопутствующей маникюрной продукции.</p>
+			</div>
+			<div class="company-block-list">
+				<h1 class="company-block-list-head">
+					<span class="company-block-list-head-span top">КАЧЕСТВО</span>
+					<span class="company-block-list-head-span bottom">ПРЕВЫШЕ ВСЕГО</span>
+				</h1>
+				<ul class="company-block-list-rows">
+					<li class="company-row">Продукция LiNTO™ имеет Европейскую CPNP-нотификацию.</li>
+					<li class="company-row">В основе продукции LiNTO™ лежат инновационные разработки Европейских инженеров-технологов, трудившихся несколько лет над созданием запатентованной формулы нашего гель-лака для ногтей, делающего его текстуру уникальной.</li>
+					<li class="company-row">Вся продукция, представленная в ассортименте нашей компании, производится в Европе на современном высокотехнологическом оборудовании с использованием сертифицированного европейского сырья и материалов.</li>
+					<li class="company-row">Наша продукция PREMIUM класса соответствует самым высоким стандартам качества, что подтверждается необходимыми декларациями соответствия и протоколами испытаний.</li>
+					<li class="company-row">Заказывая маникюрную продукцию компании LiNTO™ вы обязательно получите официальные сертификаты качества на все приобретенные товары, а также заключение о гипоаллергенности гель-лаков и базовых покрытий.</li>
+					<li class="company-row">Производство Latvia.</li>
+				</ul>
+			</div>
+			<p class="company-info">Мы занимаемся оптовой и розничной продажей продукции европейского бренда LiNTO™ по всему миру.</p>
+		</div>
+	</div>
+</section>
+<?php include './elements/footer.php'; ?>
