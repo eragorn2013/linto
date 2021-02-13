aeade14dcaa1fa1ca89425413c89dfeaa45ace76
nemo <eragorn2013@yandex.ru>
2021-02-13 14:54:45 +0300

    Доделана страница клуба

--- club.php
+++ club.php
@@ -12,7 +12,7 @@
 		<div class="content club-program-content">
 			<div class="club-program-text">
 				<h1 class="club-program-text-head">КЛУБНАЯ ПРОГРАММА</h1>
-				<p class="club-program-text-name">CLUB<br>LiNTO<span class="club-program-text-name-tm">™</span></p>
+				<p class="club-program-text-name">CLUB<br><span class="club-program-text-name-linto">LiNTO</span></p>
 				<p class="club-program-text-p">Специальные привилегии, дополнительные скидки и уникальные предложения - всё это только для участников клубной программы «Club LiNTO™»</p>
 			</div>
 			<div class="club-program-pic">
@@ -63,7 +63,7 @@
 <section id="club-privilege">
 	<div class="container">
 		<div class="content club-privilege-content">
-			<h2 class="club-privilege-head">ПРИВИЛЕГИИ<br>КЛУБА LINTO™</h2>
+			<h2 class="club-privilege-head">ПРИВИЛЕГИИ <br class="club-privilege-head-br">КЛУБА LINTO™</h2>
 			<div class="club-privilege-items">
 				<div class="club-privilege-item">
 					<i class="club-privilege-icon gift"></i>
@@ -171,6 +171,101 @@
 				<div class="club-adv-desc-text">
 					<p class="club-adv-desc-text-head">А ТАКЖЕ УЧАСТИЕ В КОНКУРСАХ ОТ КОМПАНИИ LINTO™️ ДЛЯ ДЕРЖАТЕЛЕЙ КЛУБНЫХ КАРТ</p>
 					<p class="club-adv-desc-text-p">Подарки в виде материалов, приглашений на LiNTO-FEST, возможность прохождения обучения в центре LiNTO™️ в Москве, для победителей конкурсов прохождение обучения у лучших преподавателей в России</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="club-card">
+	<div class="container">
+		<div class="content club-card-content">
+			<div class="club-card-text">
+				<h2 class="club-card-text-head">У ВАС УЖЕ <b>ЕСТЬ КАРТА?</b></h2>
+				<p class="club-card-text-p">Обратите внимание, что перед использованием карта должна быть <b>обязательно активирована</b></p>
+				<div class="club-text-button-wrap">
+					<a class="club-text-button" href="#">АКТИВИРОВАТЬ КАРТУ</a>
+				</div>
+			</div>
+			<div class="club-card-pic">
+				<img class="club-card-pic-img" src="img_club/2.png" alt="У вас уже есть карта?">
+			</div>
+		</div>
+	</div>
+</section>
+<section id="club-free">
+	<div class="container">
+		<div class="content club-free-content">
+			<div class="club-free-pic">
+				<img class="club-free-pic-img" src="img_club/3.png">
+			</div>
+			<div class="club-free-text">
+				<h2 class="club-free-text-head"><b>БЕСПЛАТНЫЙ ТЕСТ-ДРАЙВ</b> ПЕРЕД ПОКУПКОЙ</h2>
+				<p class="club-free-text-p">Вы можете ознакомиться с продукцией в нашем центральном офисе:</p>
+				<a class="club-free-text-address" href="https://yandex.ru/maps/213/moscow/?ll=37.535062%2C55.747622&mode=search&oid=29446974388&ol=biz&sll=36.734493%2C55.386160&source=wizgeo&sspn=1.972046%2C0.638721&text=%D0%B3.%20%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0%2C%20%D0%9F%D1%80%D0%B5%D1%81%D0%BD%D0%B5%D0%BD%D1%81%D0%BA%D0%B0%D1%8F%20%D0%BD%D0%B0%D0%B1.%2C%20%D0%B4.%2010%D1%812%2C%20%22%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0-%D0%A1%D0%B8%D1%82%D0%B8%22%20IQ-%D0%9A%D0%B2%D0%B0%D1%80%D1%82%D0%B0%D0%BB%20%28%D0%91%D0%B0%D1%88%D0%BD%D1%8F%20%233%29%2C%2019%20%D1%8D%D1%82%D0%B0%D0%B6%2C%20%D0%BE%D1%84%D0%B8%D1%81%20182&utm_medium=maps-desktop&utm_source=serp&z=16" target="_blank">
+					<i class="club-free-text-address-icon"></i>
+					<span class="club-free-text-address-p"><b>г. Москва, Пресненская наб., д. 10с2, "Москва-Сити"</b> IQ-Квартал (Башня #3), 19 этаж, офис 182</span>
+				</a>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="club-answers">
+	<div class="container">
+		<div class="content club-answers-content">
+			<h2 class="club-answers-head">ВОПРОС-ОТВЕТ</h2>
+			<div class="club-answers-list">
+				<div class="club-answers-item">
+					<label>
+						<input class="club-answers-input" type="checkbox" name="club-answers-input">
+						<span class="club-answers-question">
+							<span class="club-answers-question-span">КАК МОЖНО ПОЛУЧИТЬ КАРТУ ПОСЛЕ ОНЛАЙН ЗАКАЗА? КАК МОЖНО ПОЛУЧИТЬ КАРТУ ПОСЛЕ ОНЛАЙН ЗАКАЗА?</span>
+							<i class="club-answers-open"></i>
+						</span>
+						<span class="club-answers-answer">
+							<span class="club-answers-answer-text">Мы продолжаем Вас радовать нашими совместными мероприятиями с ведущими преподавателями России. В центре LiNTO™ — Москва 22, 23 и 25 октября пройдет Авторский курс Анастасии Тугашевой — «Повышение квалификации по комбинированному маникюру». Простая и безопасная техника комби одной фрезой. Курс предназначен для работающих мастеров, уверенно владеющих аппаратом.</span>
+						</span>
+					</label>
+				</div>
+				<div class="club-answers-item">
+					<label>
+						<input class="club-answers-input" type="checkbox" name="club-answers-input">
+						<span class="club-answers-question">
+							<span class="club-answers-question-span">СКОЛЬКО ДЕЙСТВУЕТ КАРТА?</span>
+							<i class="club-answers-open"></i>
+						</span>
+						<span class="club-answers-answer">
+							<span class="club-answers-answer-text">Мы продолжаем Вас радовать нашими совместными мероприятиями с ведущими преподавателями России. В центре LiNTO™ — Москва 22, 23 и 25 октября пройдет Авторский курс Анастасии Тугашевой — «Повышение квалификации по комбинированному маникюру». Простая и безопасная техника комби одной фрезой. Курс предназначен для работающих мастеров, уверенно владеющих аппаратом.</span>
+							<span class="club-answers-answer-text">Мы продолжаем Вас радовать нашими совместными мероприятиями с ведущими преподавателями России. В центре LiNTO™ — Москва 22, 23 и 25 октября пройдет Авторский курс Анастасии Тугашевой — «Повышение квалификации по комбинированному маникюру». Простая и безопасная техника комби одной фрезой. Курс предназначен для работающих мастеров, уверенно владеющих аппаратом.</span>
+						</span>
+					</label>
+				</div>
+				<div class="club-answers-item">
+					<label>
+						<input class="club-answers-input" type="checkbox" name="club-answers-input">
+						<span class="club-answers-question">
+							<span class="club-answers-question-span">КАК МОЖНО ПОЛУЧИТЬ КАРТУ ПОСЛЕ ОНЛАЙН ЗАКАЗА?</span>
+							<i class="club-answers-open"></i>
+						</span>
+						<span class="club-answers-answer">
+							<span class="club-answers-answer-text">Мы продолжаем Вас радовать нашими совместными мероприятиями с ведущими преподавателями России. В центре LiNTO™ — Москва 22, 23 и 25 октября пройдет Авторский курс Анастасии Тугашевой — «Повышение квалификации по комбинированному маникюру». Простая и безопасная техника комби одной фрезой. Курс предназначен для работающих мастеров, уверенно владеющих аппаратом.</span>
+						</span>
+					</label>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="club-get">
+	<div class="container">
+		<div class="content">
+			<div class="club-get-pic">
+				<img class="club-get-pic-img" src="img_club/4.png" alt="СТАНЬ ЧЛЕНОМ КЛУБА LiNTO™️"></img>
+			</div>
+			<div class="club-get-text">
+				<h2 class="club-get-text-head">СТАНЬ ЧЛЕНОМ <b>КЛУБА LiNTO™️</b></h2>
+				<p class="club-get-text-p">и получи персональную клубную карту прямо сейчас</p>
+				<div class="club-text-button-wrap">
+					<a class="club-text-button" href="#">ПЕРЕЙТИ В КАТАЛОГ</a>
 				</div>
 			</div>
 		</div>
